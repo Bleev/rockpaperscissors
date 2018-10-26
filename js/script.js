b2a1e3e63b563604e227cbb90316257b2c6bd829
Bryan Vo <derpynoobking@gmail.com>
2018-10-26 01:34:10 +0000

    Your message heree

--- js/script.js
+++ js/script.js
@@ -8,4 +8,48 @@
 
 
 // DOCUMENT READY FUNCTION BELOW
-
+var userChoice;
+var computerChoice;
+var winner;
+var randomNumber = 0;
+$("#shoot").click(function(){
+    $("#userChoice").text($("#input").val());
+    var randomNumber = Math.random();
+    var userChoice = $("#input").val(); 
+    $("#input").html(userChoice);
+    if (randomNumber <=.33) {
+        $("#computerChoice").html("Rock");
+        var computerChoice = "Rock";
+    }
+    else if (randomNumber >.33 && randomNumber <= .66) {
+        $("#computerChoice").html("Paper");
+    }
+    else  {
+        $("#computerChoice").html("Scissors");
+    }
+    if (userChoice === "Scissors" && computerChoice === "Rock") {
+        alert("Computer Wins!");
+    }
+    else if (userChoice === "Scissors" && computerChoice === "Paper") {
+        alert("Human Wins!");
+    }else if (userChoice === "Scissors" && computerChoice === "Scissors") {
+        alert("Tie!");
+    }else if (userChoice === "Rock" && computerChoice === "Rock") {
+        alert("Tie!");
+    }else if (userChoice === "Rock" && computerChoice === "Paper") {
+        alert("Computer Wins!");
+    }else if (userChoice === "Rock" && computerChoice === "Scissors") {
+        alert("Human Wins!");
+    }else if (userChoice === "Paper" && computerChoice === "Rock") {
+        alert("Human Wins!");
+    }
+    else if (userChoice === "Paper" && computerChoice === "Paper") {
+        alert("Tie!");
+    }
+    else if (userChoice === "Paper" && computerChoice === "Scissors") {
+        alert("Computer Wins!");
+    }
+    else {
+        alert("Please capitalize your first letter only, or invalid choice!")
+    }
+});
